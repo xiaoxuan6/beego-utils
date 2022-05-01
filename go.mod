61c81f702eaf960937ba10a7223c17c8de274644
xiaoxuan6 <1527736751@qq.com>
2022-05-01 20:45:48 +0800

    Add captcha service

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/astaxie/beego v1.12.3
+	github.com/dchest/captcha v0.0.0-20200903113550-03f5f0333e1f // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
 	github.com/stretchr/testify v1.4.0
---
+++ services/captcha_service.go
@@ -0,0 +1,61 @@
+package services
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"github.com/astaxie/beego"
+	"github.com/dchest/captcha"
+)
+
+type captchaService struct {
+}
+
+type Captcha struct {
+	Id string `json:"id"` // 验证码ID
+}
+
+var length int
+var stdWidth int
+var stdHeight int
+
+func init() {
+	length = beego.AppConfig.DefaultInt("len", captcha.DefaultLen)
+	stdWidth = beego.AppConfig.DefaultInt("std_width", captcha.StdWidth)
+	stdHeight = beego.AppConfig.DefaultInt("std_height", captcha.StdHeight)
+}
+
+var CaptchaService = newCaptchaService()
+
+func newCaptchaService() *captchaService {
+	return new(captchaService)
+}
+
+func (c *captchaService) NewCaptcha() (cap *Captcha) {
+	captchaId := captcha.NewLen(length)
+
+	cap = &Captcha{
+		captchaId,
+	}
+
+	return cap
+}
+
+func (c *captchaService) Verify(captchaId, captchaVal string) error {
+	if ok := captcha.VerifyString(captchaId, captchaVal); !ok {
+		return fmt.Errorf("验证失败")
+	}
+
+	return nil
+}
+
+func (c *captchaService) GetCaptchaBase64ById(captchaId string) (str string, error error) {
+	var content bytes.Buffer
+
+	if err := captcha.WriteImage(&content, captchaId, stdWidth, stdHeight); err != nil {
+		return "", err
+	}
+
+	str = base64.StdEncoding.EncodeToString(content.Bytes())
+	return str, nil
+}
